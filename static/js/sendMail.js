f9053dd5d179cff24d3ba6772472b7411904eb33
Michael Nickey <mnickey@gmail.com>
2016-02-23 20:30:55 -0800

    updated call to action to email in a new window leaving the portfolio window open as well

--- static/js/sendMail.js
+++ static/js/sendMail.js
@@ -1,8 +1,8 @@
 function sendMail() {
     var link = "mailto:mnickey@gmail.com"
-             + "?subject=" + escape("Emailing you from your portfolio")
-             + "&body=" + escape(document.getElementById('message').value)
-    ;
+            + "?subject=" + escape("Emailing you from your portfolio")
+            + "&body=" + escape(document.getElementById('message').value)
+        ;
 
-    window.location.href = link;
+    window.open(link);
 }
