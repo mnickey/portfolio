a1524699b273a54101a312df66dbc2f17e8e92e9
Michael Nickey <mnickey@gmail.com>
2016-02-23 19:34:29 -0800

    Added email call to action, updated css

---
+++ static/js/sendMail.js
@@ -0,0 +1,8 @@
+function sendMail() {
+    var link = "mailto:mnickey@gmail.com"
+             + "?subject=" + escape("Emailing you from your portfolio")
+             + "&body=" + escape(document.getElementById('message').value)
+    ;
+
+    window.location.href = link;
+}
